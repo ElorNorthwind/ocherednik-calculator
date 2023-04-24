b0600efe13264696e7cef36a2dc4cc3997fd21a5
Lorans <elornorthwind@gmail.com>
2023-04-24 11:09:04 +0300

    icon sizing bug

--- src/entity/Calculator/ui/Results.tsx
+++ src/entity/Calculator/ui/Results.tsx
@@ -18,7 +18,6 @@ function Results(props: ResultsProps) {
       iconClass="stroke-orange-800"
       hasBg={true}
       className="hidden md:block"
-      size={24}
     />
   );
 
--- src/shared/ui/AppIcon/AppIcon.tsx
+++ src/shared/ui/AppIcon/AppIcon.tsx
@@ -1,14 +1,14 @@
 interface AppIconProps {
   Svg: React.FC<React.SVGProps<SVGSVGElement>>;
-  hasBg?: boolean;
   size?: number;
+  hasBg?: boolean;
   iconClass?: string;
   bgClass?: string;
   className?: string;
 }
 
 export default function AppIcon(props: AppIconProps) {
-  const { Svg, hasBg = true, size = 24, iconClass = "stroke-red-800", bgClass = "bg-red-200", className = "" } = props;
+  const { Svg, size = 24, hasBg = true, iconClass = "stroke-red-800", bgClass = "bg-red-200", className = "" } = props;
 
   if (!hasBg) {
     return <Svg className={`relative h-${size} w-${size} stroke-[0.3] ${iconClass} ${className}`} />;
@@ -16,8 +16,8 @@ export default function AppIcon(props: AppIconProps) {
 
   return (
     <div className={`relative h-${size} w-${size} pt-1 pl-1 ${className}`}>
-      <div className={`z-0 absolute inset-1 rounded-full ${bgClass} `} />
-      <Svg className={`z-1 relative h-full w-full stroke-[0.3] ${iconClass}`} />
+      <div className={`z-0 absolute inset-0 rounded-full ${bgClass} `} />
+      <Svg className={`z-1 relative h-${size} w-${size} stroke-[0.3] ${iconClass}`} />
     </div>
   );
 }
