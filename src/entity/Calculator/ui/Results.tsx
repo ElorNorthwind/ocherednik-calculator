e13541cb9c6ac1a63074448e61a452eaccb55a00
Lorans <elornorthwind@gmail.com>
2023-04-27 13:26:19 +0300

    some result animations

--- src/entity/Calculator/ui/Results.tsx
+++ src/entity/Calculator/ui/Results.tsx
@@ -14,40 +14,48 @@ function Results(props: ResultsProps) {
   const icon = (
     <AppIcon
       Svg={fullData ? PointIcon : SearchIcon}
-      bgClass="bg-orange-100 group-hover:scale-110 transition-transform duration-200"
+      bgClass="bg-orange-100"
       iconClass="stroke-orange-800"
       hasBg={true}
       className="hidden md:block print:md:hidden"
     />
   );
 
-  const text = fullData ? (
+  const text = (
     <span className={`flex justify-between flex-col w-full`}>
-      <span className={`block print:text-right`}>Примерная стоимость помещения:</span>
       <span
-        className={`block text-4xl text-red-600 whitespace-nowrap print:text-right transition-opacity duration-500 ${
+        className={`block absoulte print:text-right transition-all duration-300 ${
+          fullData ? "-translate-y-6" : "translate-y-0"
+        }`}
+      >
+        {fullData ? "Примерная стоимость помещения:" : "Заполните характеристики для расчёта примерной стоимости"}
+      </span>
+      <span
+        className={`absolute block text-4xl text-red-600 whitespace-nowrap print:text-right transition-opacity delay-100 duration-300 ${
           fullData ? "opacity-100" : "opacity-0"
         }`}
       >
-        {"≈ "}
-        {new Intl.NumberFormat("ru-RU", {
-          style: "currency",
-          currency: "RUB",
-        }).format(result)}
+        {fullData
+          ? "≈ " +
+            new Intl.NumberFormat("ru-RU", {
+              style: "currency",
+              currency: "RUB",
+            }).format(result)
+          : ""}
       </span>
     </span>
-  ) : (
-    <span className={`w-full`}>Заполните характеристики для расчёта примерной стоимости</span>
   );
 
   return (
     <div
-      className={`${className} relative group w-full mt-3 p-6 bg-orange-50 grid grid-cols-1 md:grid-cols-[max-content_1fr] items-center gap-3 md:gap-6 overflow-hidden print:border-t print:border-stone-400`}
+      className={`${className} min-h-28 relative group w-full mt-3 p-6 bg-orange-50 grid grid-cols-1 md:grid-cols-[max-content_1fr] items-center gap-3 md:gap-6 overflow-hidden print:border-t print:border-stone-400`}
     >
-      <PrinterIcon
-        className="z-3 h-12 w-12 absolute right-3 top-3 stroke-orange-200 stroke-[1.6] hidden print:opacity-0 group-hover:block cursor-pointer"
-        onClick={() => window.print()}
-      />
+      {fullData && (
+        <PrinterIcon
+          className="z-3 h-12 w-12 absolute right-3 top-3 stroke-orange-200 stroke-[1.6] hidden print:opacity-0 group-hover:block cursor-pointer"
+          onClick={() => window.print()}
+        />
+      )}
       {icon}
       {text}
     </div>
